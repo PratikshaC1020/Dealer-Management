cec786f8b0097fb0f0be42ca05de3db74544d310
amishachavan092002 <amishachavan092002@gmail.com>
2026-03-12 23:10:14 +0530

    master model class

---
+++ BespokeSoftware/Models/PaymentMode.cs
@@ -0,0 +1,11 @@
+﻿namespace BespokeSoftware.Models
+{
+    public class PaymentMode
+    {
+        public int PaymentModeID { get; set; }
+        public string Payment { get; set; }
+        public bool IsDelete { get; set; }
+
+        public List<PaymentMode> PaymentModeList { get; set; }
+    }
+}
